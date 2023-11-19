d7b697dcab9265996925bf52cc86634731752451
Aureprz <aurelien.91440@gmail.com>
2023-11-19 01:03:57 +0100

    dict_pres5

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from Fonctions import*
 name_list = os.listdir("speeches-20231116")
-prenompres5 = ["Emmanuel", "François", "Nicolas", "Jacques", "François", "Valéry", "Georges", "Charles"]
+dict_pres5 = {"Macron": "Emmanuel", "Hollande": "François", "Sarkozy": "Nicolas", "Chirac": "Jacques",
+              "Mitterrand": "François", "Giscard dEstaing": "Valéry", "Pompidou": "Georges",
+              "de Gaulle": "Charles"}
 prenompres4 = ["René", "Vincent"]
 prenompres3 = ["Albert", "Paul", "Gaston", "Alexandre", "Paul", "Raymond", "Armand", "Émile", "Félix", "Jean", "Sadi",
                "Jules", "Patrice", "Adolphe"]
