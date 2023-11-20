39ea5ef90a503b8677da4b8bd6c72180f6796a5f
Aureprz <aurelien.91440@gmail.com>
2023-11-20 13:11:00 +0100

    mise en form

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
+########################################################################################################################
+# importing external functions
+########################################################################################################################
 import os
 from Fonctions import*
-directory = "./speeches-20231116"
-files_names = list_of_files(directory, "txt")
-dict_pres5 = {"Macron": "Emmanuel", "Hollande": "François", "Sarkozy": "Nicolas", "Chirac": "Jacques",
-              "Mitterrand": "François", "Giscard dEstaing": "Valéry", "Pompidou": "Georges",
-              "de Gaulle": "Charles"}
-prenompres4 = {"Coty": "René", "Auriol": "Vincent"}
-prenompres3 = {"Lebrun": "Albert", "Doumer": "Paul", "Doumergue": "Gaston", "Millerand": "Alexandre", "Deschanel": "Paul", "Poincaré": "Raymond", "Fallières": "Armand", "Loubet": "Émile", "Faure": "Félix", "Casimir-Perier": "Jean", "Carnot": "Sadi",
-               "Grévy": "Jules", "de Mac Mahon": "Patrice", "Thiers": "Adolphe"}
+
+########################################################################################################################
+# VARIABLES
+########################################################################################################################
+# directory containing all the files to analyze
+directory_base = "./speeches-20231116"
+directory_clear = "./cleaned"
+
+# list of file names
+list_files_names = list_of_files(directory_base, "txt")
+
+# dictionary of president dictionary based on the current republic
+dict_pres = {"p5": {"Macron": "Emmanuel", "Hollande": "François", "Sarkozy": "Nicolas", "Chirac": "Jacques",
+                    "Mitterrand": "François", "Giscard dEstaing": "Valéry", "Pompidou": "Georges",
+                    "de Gaulle": "Charles"},
+             "p4": {"Coty": "René", "Auriol": "Vincent"},
+             "p3": {"Lebrun": "Albert", "Doumer": "Paul", "Doumergue": "Gaston", "Millerand": "Alexandre",
+                    "Deschanel": "Paul", "Poincaré": "Raymond", "Fallières": "Armand", "Loubet": "Émile",
+                    "Faure": "Félix", "Casimir-Perier": "Jean", "Carnot": "Sadi", "Grévy": "Jules",
+                    "de Mac Mahon": "Patrice", "Thiers": "Adolphe"}}
+
+# list of punctuation sign
 list_punctuation = [
-    '.', ',', ';', ':', '!', '?', '(', ')', '[', ']', '{', '}', '<', '>', "'", '"', '/',
-    '|', '@', '#', '$', '%', '^', '&', '*', '_', '+', '-', '=', '~', '`', "\n"]
+                    '.', ',', ';', ':', '!', '?', '(', ')', '[', ']', '{', '}', '<', '>', "'", '"', '/',
+                    '|', '@', '#', '$', '%', '^', '&', '*', '_', '+', '-', '=', '~', '`', "\n"
+                    ]
+
+# list of stopword (not important word)
 list_stopword = []
-print(files_names)
-#recupère la list_stopword
+
+########################################################################################################################
+# MAIN PROGRAM
+########################################################################################################################
+# recover the stop words list in the folder
 with open("stop_words_french.txt", "r") as f1:
     for line in f1:
         list_stopword.append(line[:-1])
 
-punctuation("./speeches-20231116", "./cleaned", "/Nomination_Chirac1.txt", list_punctuation)
+punctuation(directory_base, directory_clear, "/Nomination_Chirac1.txt", list_punctuation)
 minuscule("cleaned/Nomination_Chirac1.txt")
 stopword("cleaned/Nomination_Chirac1.txt", list_stopword)
