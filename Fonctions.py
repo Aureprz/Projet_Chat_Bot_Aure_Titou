21451b6588c638b5573d2d3fee8e505ea25bae7d
Aureprz <aurelien.91440@gmail.com>
2023-11-24 17:27:57 +0100

    com to doc

--- Fonctions.py
+++ Fonctions.py
@@ -2,8 +2,8 @@ from math import log10
 import os
 
 
-# function to extract the name of each file (speech)
 def extraire_nom(list_names_files):
+    """function to extract the name of each file (speech)"""
     list_nom = set()
     for file_name in list_names_files:
         # Remove "Nomination_" et ".txt"
@@ -15,8 +15,8 @@ def extraire_nom(list_names_files):
     return list(list_nom)
 
 
-# function removing each punctuation element, such as commas or hyphens, from files
 def punctuation(file_path, list_punctuation):
+    """ function removing each punctuation element, such as commas or hyphens, from files"""
     with open(file_path, "r") as f1:
         txt = f1.read()
         translation_table = str.maketrans(list_punctuation, " " * len(list_punctuation))
@@ -27,8 +27,8 @@ def punctuation(file_path, list_punctuation):
         f1.write(txt)
 
 
-# function to remove words that appear too often (defined by a predefined list)
 def stopword(file_path, list_stopword):
+    """function to remove words that appear too often (defined by a predefined list)"""
     text_c = []
     with open(file_path, "r") as f1:
         text = f1.readline().split()
@@ -37,8 +37,8 @@ def stopword(file_path, list_stopword):
         f1.write(text_c)
 
 
-# function transforming the text of each file into lowercase letters
 def minuscule(file_path):
+    """function transforming the text of each file into lowercase letters"""
     with open(file_path, "r") as f1:
         txt = f1.read()
     translation_table = str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz")
@@ -47,8 +47,8 @@ def minuscule(file_path):
         f1.write(txt)
 
 
-# TF function calculating the frequency of occurrence of a term in such file
 def term_frequency(name_files_cleaned, dict_word):
+    """TF function calculating the frequency of occurrence of a term in such file"""
     with open(name_files_cleaned, "r") as f1:
         list_txt = f1.read().split(" ")
     for i in list_txt:
@@ -56,8 +56,8 @@ def term_frequency(name_files_cleaned, dict_word):
     return dict_word
 
 
-# IDF function calculating the importance of a term across all existing files
 def inverse_document_frequency(list_dict_term, dict_word):
+    """IDF function calculating the importance of a term across all existing files"""
     for dict_term in list_dict_term:
         for word in dict_term.keys():
             dict_word[word] = dict_word[word] + 1
@@ -69,8 +69,8 @@ def inverse_document_frequency(list_dict_term, dict_word):
     return dict_word
 
 
-# function giving the list of first and last names associated with each president
 def noms_prenoms(dict_identity, list_noms):
+    """function giving the list of first and last names associated with each president"""
     dict_presidents = {i: dict_identity[i] for i in list_noms}
     return dict_presidents
 
@@ -89,8 +89,8 @@ def copy_directory(directory1, directory2):
             f2.write(f1.read())
 
 
-# verifier tf_idf
 def tf_idf(dic_tf, idf):
+    """verifier tf_idf"""
     dic_tf_idf = {i: {j: dic_tf[i][j] * idf[j] for j in dic_tf[i].keys()} for i in dic_tf.keys()}
     return dic_tf_idf
 
