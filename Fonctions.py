f4ca61a7848acf5aab23b75554be4f75218b60e4
Aureprz <aurelien.91440@gmail.com>
2023-12-01 12:56:19 +0100

    create tf_idf.py fill Menu.py fill tf_idf.py

--- Fonctions.py
+++ Fonctions.py
@@ -1,4 +1,4 @@
-from math import log10
+
 import os
 
 
@@ -13,65 +13,6 @@ def extraire_nom(list_names_files):
             j -= 1
         list_nom.append(name[:j+1])
     return list(list_nom)
-
-
-def punctuation(file_path, list_punctuation):
-    """ function removing each punctuation element, such as commas or hyphens, from files"""
-    list_punctuation = "".join(list_punctuation)
-    with open(file_path, "r", encoding='utf-8') as f1:
-        txt = f1.read()
-        translation_table = str.maketrans(list_punctuation, " " * len(list_punctuation))
-        txt = txt.translate(translation_table)
-        txt = " ".join(txt.split())
-
-    with open(file_path, "w", encoding='utf-8') as f1:
-        f1.write(txt)
-
-
-def stopword(file_path, list_stopword):
-    """function to remove words that appear too often (defined by a predefined list called list_stopword)"""
-    with open(file_path, "r", encoding='utf-8') as f1:
-        text = f1.readline().split()
-        text_c = " ".join(word for word in text if word not in list_stopword)
-    with open(file_path, "w", encoding='utf-8') as f1:
-        f1.write(text_c)
-
-
-def minuscule(file_path):
-    """function transforming the text of each file into lowercase letters"""
-    with open(file_path, "r", encoding='utf-8') as f1:
-        txt = f1.read()
-    translation_table = str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz")
-    txt = txt.translate(translation_table)
-    with open(file_path, "w", encoding='utf-8') as f1:
-        f1.write(txt)
-
-
-def term_frequency(name_file_cleaned, dict_word):
-    """TF function calculating the frequency of occurrence of a term in such file"""
-    score_tf = dict_word.copy()
-    with open(name_file_cleaned, "r", encoding='utf-8') as f1:
-        list_txt = f1.read().split(" ")
-    for i in list_txt:
-        score_tf[i] = score_tf[i]+1
-    for word, val in score_tf.items():
-        score_tf[word] /= len(list_txt)
-
-    return score_tf
-
-
-def inverse_document_frequency(list_dict_term, dict_word):
-    """IDF function calculating the importance of a term across all existing files"""
-    score_tf = dict_word.copy()
-    for dict_term in list_dict_term.values():
-        for word in dict_term.keys():
-            if dict_term[word] > 0:
-                score_tf[word] = score_tf[word] + 1
-
-    nb_documents = len(list_dict_term)
-    for i in score_tf:
-        score_tf[i] = log10(nb_documents / (score_tf[i]))
-    return score_tf
 
 
 def noms_prenoms(dict_identity, list_noms):
@@ -97,12 +38,6 @@ def copy_directory(directory1, directory2):
             f2.write(f1.read())
         list_directory2.append(os.path.join(directory2, files))
     return list_directory2
-
-
-def tf_idf(dic_tf, idf):
-    """function giving us the TF-IDF matrix for all the words in the files"""
-    dic_tf_idf = {i: {j: dic_tf[i][j] * idf[j] for j in dic_tf[i].keys()} for i in dic_tf.keys()}
-    return dic_tf_idf
 
 
 def dict_words(list_path_files):
